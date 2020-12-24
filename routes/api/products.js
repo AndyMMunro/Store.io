392ef0ed0ae981a67a94757b62929ac38471fa53
AndyMMunro <agovitadventures@gmail.com>
2020-12-23 20:47:11 -0800

    product route displays the porduct json element in localhost:3001/products

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,38 +1,32 @@
-var db = require("../../models");
+const db = require("../../models");
+const express = require("express");
+const router = express.Router();
 
-module.exports = function(app) {
-  app.get("/products", function(req, res) {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-    db.Products.findAll({
-      include: [db.Post]
-    }).then(function(dbProducts) {
+module.exports = function(router) {
+
+  router.get("/products", function(req, res) {
+    db.products.findAll().then(function(dbProducts) {
       res.json(dbProducts);
     });
   });
 
-  // app.get("/api/products/:id", function(req, res) {
-  //   // Here we add an "include" property to our options in our findOne query
-  //   // We set the value to an array of the models we want to include in a left outer join
-  //   // In this case, just db.Post
+  // router.get("/products/:id", function(req, res) {
   //   db.Products.findOne({
   //     where: {
   //       id: req.params.id
-  //     },
-  //     include: [db.Post]
+  //     }
   //   }).then(function(dbProducts) {
   //     res.json(dbProducts);
   //   });
   // });
 
-  // app.post("/api/products", function(req, res) {
+  // router.post("/products", function(req, res) {
   //   db.Products.create(req.body).then(function(dbProducts) {
   //     res.json(dbProducts);
   //   });
   // });
 
-  // app.delete("/api/products/:id", function(req, res) {
+  // router.delete("/products/:id", function(req, res) {
   //   db.Products.destroy({
   //     where: {
   //       id: req.params.id
