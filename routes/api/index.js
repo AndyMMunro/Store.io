04cbcea44af95a363af35967d7dde0f675f6778e
AndyMMunro <agovitadventures@gmail.com>
2020-12-31 16:04:14 -0800

    removed controller indexes

--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const productRoutes = require("./products");
-
-
-// Book routes end points what how in the URL
-router.use("/products", productRoutes);
-
-module.exports = router;
-
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // const { Router } = require("express");
-// const { Router } = require("express");
 const express = require("express");
 // const routes = require("./routes");
 const app = express();
