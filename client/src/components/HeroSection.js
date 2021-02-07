b4f68664e49c37a60ceef7a3b744ceb03b85643d
AndyMMunro <agovitadventures@gmail.com>
2021-02-07 11:32:40 -0800

    changed home page to be more user friendly

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -7,24 +7,17 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/friendswalking.mp4' autoPlay loop muted />
-      <h1>Fashion AWAITS</h1>
-      <p>What are you waiting for?</p>
+      <h1>Clothing and Gear</h1>
+      <p>Git on it</p>
       <div className='hero-btns'>
         <Button
           className='btns'
           buttonStyle='btn--outline'
           buttonSize='btn--large'
         >
-          GET STARTED
+          Shop Now
         </Button>
-        <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          WATCH TRAILER <i className='far fa-play-circle' />
-        </Button>
+
       </div>
     </div>
   );
